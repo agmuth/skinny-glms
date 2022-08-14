8030357474702f760f2c3f1d8525af8ae2770f36
Andrew Muth <agmuth@gmail.com>
2022-08-14 12:35:22 -0700

    added non canonical link support

--- skinny_binomial.py
+++ skinny_binomial.py
@@ -1,23 +1,44 @@
 from utils import *
 from skinny_lm import SkinnyLM
 
+
 class SkinnyBinomialRegressionLogitLink(SkinnyLM):
     def __init__(self):
         super().__init__()
-        self.theta_of_mu = np.vectorize(lambda mu: np.log(mu / (1 - mu)))
+        self.theta_of_mu = np.vectorize(lambda mu: np.log(safe_division(mu , (1 - mu))))
         self.mu_of_eta = np.vectorize(lambda eta: 1 / (1 + np.exp(-eta)))
 
 
+class SkinnyBinomialRegressionProbitLink(SkinnyBinomialRegressionLogitLink):
+    def __init__(self):
+        super().__init__()
+        self.mu_of_eta = np.vectorize(lambda eta: inv_probit(eta))
+
+
 if __name__ == "__main__":
-    n = 1000
+    # n = 1000
+    # p = 1
+
+    # X = np.hstack([np.ones((n, p)), np.random.normal(size=(n, p))])
+    # b = np.random.normal(size=(1, p+1))
+    # probs = 1 / (1 + np.exp(- X @ b.T) )
+    # y = np.random.binomial(1, probs, (n, 1))
+
+    # lm = SkinnyBinomialRegressionLogitLink()
+    # lm.fit(X, y)
+    # print(lm.b.flatten())
+    # print(b.flatten())
+
+
+    n = 10000
     p = 1
 
     X = np.hstack([np.ones((n, p)), np.random.normal(size=(n, p))])
     b = np.random.normal(size=(1, p+1))
-    probs = 1 / (1 + np.exp(- X @ b.T) )
+    probs = inv_probit(X @ b.T)
     y = np.random.binomial(1, probs, (n, 1))
 
-    lm = SkinnyBinomialRegressionLogitLink()
+    lm = SkinnyBinomialRegressionProbitLink()
     lm.fit(X, y)
     print(lm.b.flatten())
     print(b.flatten())
--- utils.py
+++ utils.py
@@ -1,7 +1,14 @@
 import numpy as np
+from scipy.special import erf
+
+MACHINE_EPS = np.finfo(np.float64).eps
 
 def differentiate(f, h=1e-4):
     h_inv = 1/h
     f_dx = np.vectorize(lambda x: 0.5 * h_inv * (f(x + h) - f(x - h)))
     return f_dx
+
+
+inv_probit = lambda x: 0.5 * (1 + erf(x / np.sqrt(2)))
+safe_division = np.vectorize(lambda x, y: x / y if np.abs(y) > MACHINE_EPS else np.sign(y) * x / MACHINE_EPS)
 
