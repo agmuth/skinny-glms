8815444d5f14f7778880ec358e7258a0b9391a55
Andrew Muth <agmuth@gmail.com>
2023-02-15 15:17:57 -0700

    multiplied test rate by -1

--- skinny_gamma.py
+++ skinny_gamma.py
@@ -3,8 +3,13 @@ from skinny_lm import SkinnyLM
 
 class SkinnyGammaRegressionLogLink(SkinnyLM):
     def __init__(self):
-        self.theta_of_mu = lambda mu: mu**-1
+        self.theta_of_mu = lambda mu: -mu**-1
         self.mu_of_eta = lambda eta: np.exp(eta)  # need to figure out why signs of b estimate are reversed
+
+class SkinnyGammaRegressionInverseLink(SkinnyLM):
+    def __init__(self):
+        self.theta_of_mu = lambda mu: -mu**-1
+        self.mu_of_eta = lambda eta: np.exp(eta)
     
 
 
@@ -16,7 +21,7 @@ if __name__ == "__main__":
     X = np.hstack([np.ones((n, p)), np.random.normal(size=(n, p))])
     b = np.random.normal(size=(1, p+1))
     
-    rate = np.exp(X @ b.T)
+    rate = np.exp(-1 * X @ b.T)
     y = np.random.gamma(1, 1/rate, (n, 1))
 
     lm = SkinnyGammaRegressionLogLink()
