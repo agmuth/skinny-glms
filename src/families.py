updated weights to use correct variance defn

src/families.py
@@ -1,25 +1,23 @@
 from functions import * 
 from links import BaseLink
-import numdifftools as nd
 
 class BaseFamily:
-    def __init__(self, link:BaseLink):
+    def __init__(self, link:BaseLink, canonical_link: callable=identity):
         self.link = link
+        self.canonical_link = canonical_link
+        self.canonical_link_deriv = differentiate(self.canonical_link)
+
+    def inv_variance(self, mu: np.ndarray) -> np.ndarray:
+        return self.canonical_link_deriv(mu)
 
 class GaussianFamily(BaseFamily):
     def __init__(self, link):
-        super().__init__(link)
-        self.canonical_link = identity
-        self.canonical_link_inv_deriv = differentiate(identity)
+        super().__init__(link, canonical_link = identity)
 
 class BinomialFamily(BaseFamily):
     def __init__(self, link: BaseLink):
-        super().__init__(link)
-        self.canonical_link = logit
-        self.canonical_link_inv_deriv = differentiate(sigmoid)  
+        super().__init__(link, canonical_link = logit)
 
 class GammaFamily(BaseFamily):
     def __init__(self, link: BaseLink):
-        super().__init__(link)
-        self.canonical_link = negative_inverse
-        self.canonical_link_inv_deriv = differentiate(negative_inverse) 
+        super().__init__(link, canonical_link = negative_inverse)

src/skinny_glm.py
@@ -19,7 +19,7 @@ class SkinnyGLM():
             eta_i = X @ beta_i
             m_i = self.family.link.inv_link(eta_i)  # current estimate of mu
             u_i = eta_i + self.family.link.inv_link_deriv(m_i) * (y - m_i)  # working/linearized response
-            W_i = np.diag((self.family.canonical_link_inv_deriv(m_i) * self.family.link.inv_link_deriv(m_i)**2).flatten())
+            W_i = np.diag((self.family.inv_variance(m_i) * self.family.link.inv_link_deriv(m_i)**2).flatten())
             delta_beta = self._wols(X, u_i, W_i) - beta_i
             beta_i += delta_beta
 