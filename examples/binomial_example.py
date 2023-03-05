fixed overflow issues by changing irls starting params to be just intercept model fit to mean response (ASSUMES INTERCEPT)

examples/binomial_example.py
@@ -1,12 +1,11 @@
 import skinnyglms as skinny
 import statsmodels.api as sm
 import numpy as np
-
 skinny_link = skinny.links.ProbitLink()
-sm_link = sm.genmod.families.links.probit()
+
 
 n = 1000
-p = 2
+p = 10
 
 X = np.hstack([np.ones((n, 1)), np.random.normal(size=(n, p))])
 b = np.random.normal(size=(1, p+1))
@@ -16,8 +15,6 @@ y = np.random.binomial(1, probs, (n, 1))
 skinny_model = skinny.glm.SkinnyGLM(family=skinny.families.BinomialFamily(skinny_link))
 skinny_model._irls(X, y)
 
-stats_model = sm.GLM(y, X, family=sm.families.Binomial(sm_link))
-stats_model = stats_model.fit()
 
 print(f"true parameter estimates: {b.flatten()}")
 print(f"skinny parameter estimates: {skinny_model.b.flatten()}")

examples/gamma_example.py
@@ -1,14 +1,15 @@
 import skinnyglms as skinny
 import statsmodels.api as sm
 import numpy as np
+# np.random.seed(1234)
 
 skinny_link = skinny.links.LogLink()
 sm_link = sm.genmod.families.links.log()
 
-n = 1000
-p = 1
+n = 10000
+p = 10
 
-X = np.hstack([np.ones((n, p)), np.random.normal(size=(n, p))])
+X = np.hstack([np.ones((n, 1)), np.random.normal(size=(n, p))])
 b = np.random.normal(size=(1, p+1))
 rate = skinny_link.inv_link(X @ b.T)
 y = np.random.gamma(1, 1/rate, (n, 1))
@@ -16,14 +17,8 @@ y = np.random.gamma(1, 1/rate, (n, 1))
 skinny_model = skinny.glm.SkinnyGLM(family=skinny.families.GammaFamily(skinny_link))
 skinny_model._irls(X, y)
 
-stats_model = sm.GLM(y, X, family=sm.families.Gamma(sm_link))
-stats_model = stats_model.fit()
-
 print(f"true parameter estimates: {b.flatten()}")
 print(f"skinny parameter estimates: {skinny_model.b.flatten()}")
-print(f"statsmodels parameter estimates: {stats_model.params.flatten()}")
 
-print(f"skinny dispersion estimates: {skinny_model.dispersion}")
-print(f"statsmodels dispersion estimates: {stats_model.scale}")
 
 

examples/poisson_example.py
@@ -5,8 +5,8 @@ import numpy as np
 skinny_link = skinny.links.LogLink()
 sm_link = sm.genmod.families.links.log()
 
-n = 1000
-p = 1
+n = 10000
+p = 10
 
 X = np.hstack([np.ones((n, 1)), np.random.normal(size=(n, p))])
 b = np.random.normal(size=(1, p+1))
@@ -17,14 +17,8 @@ y = np.random.poisson(lam, (n, 1))
 skinny_model = skinny.glm.SkinnyGLM(family=skinny.families.PoissonFamily(skinny_link))
 skinny_model._irls(X, y, offset)
 
-stats_model = sm.GLM(y, X, family=sm.families.Poisson(sm_link), offset=skinny_link.link(offset).flatten())
-stats_model = stats_model.fit()
-
 print(f"true parameter estimates: {b.flatten()}")
 print(f"skinny parameter estimates: {skinny_model.b.flatten()}")
-print(f"statsmodels parameter estimates: {stats_model.params.flatten()}")
 
-print(f"skinny dispersion estimates: {skinny_model.dispersion}")
-print(f"statsmodels dispersion estimates: {stats_model.scale}")
 
 

src/skinnyglms/glm.py
@@ -19,7 +19,15 @@ class SkinnyGLM():
 
         # use ols values as starting values 
         W_i = np.ones((y.shape[0], 1))
-        beta_i = self._wols(X, y, W_i)
+        # beta_i = self._wols(X, y, W_i)
+        # beta_i = self._wols(
+        #     X, 
+        #     self.family.link.link(y), 
+        #     W_i
+        # )
+        beta_i = np.zeros((X.shape[1], 1))
+        beta_i[0, 0] += self.family.link.link(y.mean())
+
 
         self.iter = 1
         while self.iter < max_iters: